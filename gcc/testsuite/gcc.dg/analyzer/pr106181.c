f51bfd9f9b7108450c326b925893c879229152f3
Tim Lange <mail@tim-lange.me>
2022-07-04 14:59:54 +0200

    My fail

--- gcc/testsuite/gcc.dg/analyzer/pr106181.c
+++ gcc/testsuite/gcc.dg/analyzer/pr106181.c
@@ -3,7 +3,7 @@
 int32_t *
 foo (int32_t x)
 {
-  return __builtin_calloc (x * 1.1, 1); /* { dg-line calloc2 } */
-  /* { dg-warning "allocated buffer size is not a multiple of the pointee's size \\\[CWE-131\\\]" "warning" { target *-*-* } assign4 } */
-  /* { dg-message "'int32_t \\*' (\\\{aka 'int \\*'\\\})? here; 'sizeof \\(int32_t (\\\{aka int\\\})?\\)' is '4'" "note" { target *-*-* } assign4 } */
+  return __builtin_calloc (x * 1.1, 1); /* { dg-line calloc } */
+  /* { dg-warning "allocated buffer size is not a multiple of the pointee's size \\\[CWE-131\\\]" "warning" { target *-*-* } calloc } */
+  /* { dg-message "'int32_t \\*' (\\\{aka 'int \\*'\\\})? here; 'sizeof \\(int32_t (\\\{aka int\\\})?\\)' is '4'" "note" { target *-*-* } calloc } */
 }
