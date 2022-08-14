fa15994d09119ade0a57006bdd5a04848f16bb25
Tim Lange <mail@tim-lange.me>
2022-08-14 17:26:40 +0200

    Fixup testcase

--- gcc/testsuite/gcc.dg/analyzer/pr106181.c
+++ gcc/testsuite/gcc.dg/analyzer/pr106181.c
@@ -1,7 +1,11 @@
 #include <stdint.h>
 
-int32_t *
-foo (int32_t x)
+void *
+foo (int x)
 {
-  return __builtin_calloc (x * 1.1, 1); /* { dg-message "operand '\[0-9efd\\.\\+\]*?' is of type 'double'" } */
+  return __builtin_calloc (x * 1.1, 1); /* { dg-line calloc } */
+
+  /* { dg-warning "use of floating point arithmetic inside the size argument might yield unexpected results" "warning" { target *-*-* } calloc } */
+  /* { dg-message "at least one operand of" "note" { target *-*-* } calloc } */
+  /* { dg-message "only use operands of a type that represents whole numbers inside the size argument" "note" { target *-*-* } calloc } */
 }
