cb283f008adf39b8944dc0605bde895b4fa4b515
Tim Lange <mail@tim-lange.me>
2022-08-17 11:19:37 +0200

    Change err msg

--- gcc/testsuite/gcc.dg/analyzer/pr106181.c
+++ gcc/testsuite/gcc.dg/analyzer/pr106181.c
@@ -5,7 +5,7 @@ foo (int x)
 {
   return __builtin_calloc (x * 1.1, 1); /* { dg-line calloc } */
 
-  /* { dg-warning "use of floating point arithmetic inside the size argument might yield unexpected results" "warning" { target *-*-* } calloc } */
+  /* { dg-warning "use of floating-point arithmetic here might yield unexpected results" "warning" { target *-*-* } calloc } */
   /* { dg-message "operand '\(\\d|e|f|\\.|\\+|\)+' is of type 'double'" "note" { target *-*-* } calloc } */
-  /* { dg-message "only use operands of a type that represents whole numbers inside the size argument" "note" { target *-*-* } calloc } */
+  /* { dg-message "only use operands of an integer type inside the size argument" "note" { target *-*-* } calloc } */
 }
