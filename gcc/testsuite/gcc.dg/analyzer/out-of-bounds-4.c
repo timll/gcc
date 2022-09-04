39552a0b9f836d6172c6f51db8ba141418df31ec
Tim Lange <mail@tim-lange.me>
2022-09-04 21:05:57 +0200

    adapt testcase

--- gcc/testsuite/gcc.dg/analyzer/out-of-bounds-4.c
+++ gcc/testsuite/gcc.dg/analyzer/out-of-bounds-4.c
@@ -1,7 +1,5 @@
 /* { dg-additional-options "-Wno-stringop-overflow -Wno-stringop-truncation" } */
-#include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
 
 /* Wanalyzer-out-of-bounds tests for strpy-related overflows.
   
@@ -27,10 +25,10 @@ void test3 (void)
 {
   char *src = "Hello";
   char dst[5];
-  strcpy (dst, src); /* { dg-line test5 } */
+  strcpy (dst, src); /* { dg-line test3 } */
 
-  /* { dg-warning "overflow" "warning" { target *-*-* } test5 } */
-  /* { dg-message "dst" "note" { target *-*-* } test5 } */
+  /* { dg-warning "overflow" "warning" { target *-*-* } test3 } */
+  /* { dg-message "dst" "note" { target *-*-* } test3 } */
 }
 
 void test4 (void)
@@ -40,26 +38,26 @@ void test4 (void)
   strcpy (dst, src);
 }
 
-char *return_hello (void)
+const char *return_hello (void)
 {
   return "hello";
 }
 
 void test5 (void)
 {
-  char *str = return_hello ();
+  const char *str = return_hello ();
   if (!str)
     return;
   char dst[5];
-  strcpy (dst, str); /* { dg-line test9 } */
+  strcpy (dst, str); /* { dg-line test5 } */
 
-  /* { dg-warning "overflow" "warning" { target *-*-* } test9 } */
-  /* { dg-message "dst" "note" { target *-*-* } test9 } */
+  /* { dg-warning "overflow" "warning" { target *-*-* } test5 } */
+  /* { dg-message "dst" "note" { target *-*-* } test5 } */
 }
 
 void test6 (void)
 {
-  char *str = return_hello ();
+  const char *str = return_hello ();
   if (!str)
     return;
   char dst[6];
