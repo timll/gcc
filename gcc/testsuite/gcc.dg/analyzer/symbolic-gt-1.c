a4eabc6202f7925fdc2fb6a89f36bd1719ba2bdc
Tim Lange <mail@tim-lange.me>
2022-09-06 20:45:06 +0200

    test coverage for last bug

--- gcc/testsuite/gcc.dg/analyzer/symbolic-gt-1.c
+++ gcc/testsuite/gcc.dg/analyzer/symbolic-gt-1.c
@@ -2,6 +2,7 @@
 #include "analyzer-decls.h"
 
 /* Test GT_EXPR comparison of symbolic values.  */
+
 void test1 (size_t size)
 {
   size_t a = 4 * size + 1;
@@ -50,5 +51,26 @@ void test7 (size_t size, size_t mul)
 {
   size_t a = mul * size + 1;
   size_t b = mul * size;
+  __analyzer_eval (a > b); /* { dg-warning "TRUE" } */
+}
+
+void test8 (size_t size)
+{
+  size_t a = size - 5;
+  size_t b = size - 1;
+  __analyzer_eval (a > b); /* { dg-warning "UNKNOWN" } */
+}
+
+void test9 (size_t size)
+{
+  size_t a = size + 1;
+  size_t b = size + 2;
+  __analyzer_eval (a > b); /* { dg-warning "UNKNOWN" } */
+}
+
+void test10 (size_t size)
+{
+  size_t a = size + 2;
+  size_t b = size + 1;
   __analyzer_eval (a > b); /* { dg-warning "TRUE" } */
 }
